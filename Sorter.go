d640a31c19e46b805f5c9f4621406e87f29cf73e
Krishnapriya Pradeep <119120712+KrishnapriyaPradeep@users.noreply.github.com>
2025-05-20 18:16:35 +0530

    Add files via upload

--- Sorter.go
+++ Sorter.go
@@ -7,14 +7,6 @@ import (
 	"path/filepath"
 )
 
-type FileInfo struct {
-	Name string
-	Path string
-	Size int64
-	Perm os.FileMode
-	Ext  string
-}
-
 func insidedirectory(path string) []string {
 	var filesList []string
 	entries, error := os.ReadDir(path)
@@ -33,7 +25,36 @@ func insidedirectory(path string) []string {
 	return filesList
 }
 
+type FileInfo struct {
+	Name string
+	Path string
+	Size int64
+	Perm os.FileMode
+	Ext  string
+}
+
+func callGPT(jsonFile string) {
+	apikey := os.Getenv("OPENAI_API_KEY")
+	data, error := os.ReadFile(jsonFile)
+	if error != nil {
+		fmt.Println("There is an error")
+		return
+	}
+	prompt := fmt.Sprintf("You are a file organizer.Attached is a metadata file of files in a folder(files inside subfolders also included). You have to parse through each file, and using the informations like type, size,permissions, etc organize it based on the best logical reasons. It maybe based on File Types:Documents, Images, Audios, Videos, Others etc; File Size:100-500MB,500-1000MB, etc; Date Modified: Past Month,Past week, etc; or any other like this. The most apt reason must be chosen. Return Result in a JSON format along with the subfolder names and current path and new path of file.Here is the data:%s", string(data))
+
+	body := map[string]interface{}{
+		"model": "gpt-3.5-turbo",
+		"messages": []map[string]string{
+			{"role": "user", "content": prompt},
+		},
+	}
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <folder_path>")
+		return
+	}
 	path := os.Args[1]
 	fmt.Printf("Folder path = %v", path)
 	var entries []string
@@ -64,11 +85,11 @@ func main() {
 	}
 
 	jsonFileName := "metadatafile.json"
-	write := os.WriteFile(jsonFileName, jsonData, 0644)
-	if write != nil {
+	error = os.WriteFile(jsonFileName, jsonData, 0644)
+	if error != nil {
 		fmt.Print("There is an error!")
 	}
 
 	fmt.Printf("\nMetadata saved to '%s'\n", jsonFileName)
-
+	callGPT(jsonFileName)
 }
